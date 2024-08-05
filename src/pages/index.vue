f31dcd7f3d26f71c18decb9137674cbe799fb348
ycker <ybponio@gmail.com>
2024-08-05 18:11:18 +0800

    asbd

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,42 +1,39 @@
 <template>
   <v-app>
     <div class="h-100">
-        <v-tabs v-model="tab" bg-color="primary">
-          <v-tab value="one">MAIN</v-tab>
-          <v-tab value="two">REQUESTS</v-tab>
-          <v-tab value="three">SUPPLIERS</v-tab>
-          <v-tab value="four">DEPARTMENTS</v-tab>
-          <v-tab value="five">SETTINGS</v-tab>
-        </v-tabs>
+      <v-tabs v-model="tab" bg-color="primary">
+        <v-tab value="one">MAIN</v-tab>
+        <v-tab value="two">REQUESTS</v-tab>
+        <v-tab value="three">SUPPLIERS</v-tab>
+        <v-tab value="four">DEPARTMENTS</v-tab>
+        <v-tab value="five">SETTINGS</v-tab>
+      </v-tabs>
 
-        <v-card-text>
-          <v-tabs-window v-model="tab">
-            <v-tabs-window-item value="one">
-              <MainComp />
-            </v-tabs-window-item>
+      <v-card-text>
+        <v-tabs-window v-model="tab">
+          <v-tabs-window-item value="one">
+            <MainComp />
+          </v-tabs-window-item>
 
-            <v-tabs-window-item value="two">
-              <RequestComp />
-            </v-tabs-window-item>
+          <v-tabs-window-item value="two">
+            <RequestComp />
+          </v-tabs-window-item>
 
-            <v-tabs-window-item value="three">
-              <SupplierComp />
-            </v-tabs-window-item>
+          <v-tabs-window-item value="three">
+            <SupplierComp />
+          </v-tabs-window-item>
 
-<<<<<<< HEAD
-=======
-            <v-tabs-window-item value="three">
-              <SupplierComp />
-            </v-tabs-window-item>
+          <v-tabs-window-item value="three">
+            <SupplierComp />
+          </v-tabs-window-item>
 
->>>>>>> 01c1eec309b037dd1c076281170e2524a6afd2e7
-            <v-tabs-window-item value="four"> Three </v-tabs-window-item>
+          <v-tabs-window-item value="four"> Three </v-tabs-window-item>
 
-            <v-tabs-window-item value="five">
-              <SettingsComp />
-            </v-tabs-window-item>
-          </v-tabs-window>
-        </v-card-text>
+          <v-tabs-window-item value="five">
+            <SettingsComp />
+          </v-tabs-window-item>
+        </v-tabs-window>
+      </v-card-text>
     </div>
   </v-app>
 </template>
