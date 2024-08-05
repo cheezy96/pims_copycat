9d3e47e02e9f7dcfe314f1690897a083df525468
cheezy96 <dioniotokobeart@gmail.com>
2024-08-05 16:21:12 +0800

    mymessage

--- src/components/pims/setting/SettingsComp.vue
+++ src/components/pims/setting/SettingsComp.vue
@@ -1,13 +1,11 @@
 <template>
-    <div>
-        
-    </div>
+  <div class="d-flex justify-center">
+    <SettingsTabs />
+  </div>
 </template>
 
 <script setup>
-
+import SettingsTabs from "./SettingsTabs.vue";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/pims/setting/SettingsTabs.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container class="d-flex justify-center">
+    <v-card width="8200">
+      <v-tabs v-model="tab" bg-color="white">
+        <v-tab value="one">Others</v-tab>
+        <v-tab value="two">BAC members</v-tab>
+        <v-tab value="three">Personnel</v-tab>
+      </v-tabs>
+      <v-card-text>
+        <v-tabs-window v-model="tab">
+          <v-tabs-window-item value="one"> </v-tabs-window-item>
+
+          <v-tabs-window-item value="two">
+            <SettingsTabsBacMembers />
+          </v-tabs-window-item>
+
+          <v-tabs-window-item value="three"> Three </v-tabs-window-item>
+        </v-tabs-window>
+      </v-card-text>
+
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="orange">RESET</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="primary">Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import SettingsTabsBacMembers from "./SettingsTabsBacMembers.vue";
+
+let tab = ref(null);
+</script>
+
+<style scoped></style>
